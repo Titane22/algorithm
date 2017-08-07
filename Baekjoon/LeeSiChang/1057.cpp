a894852c0aa020531b16db9e509df96da8b28bf3
Titane22 <titane308@gmail.com>
2017-08-07 16:05:08 +0900

    BJ1057 Revised

--- Baekjoon/LeeSiChang/1057.cpp
+++ Baekjoon/LeeSiChang/1057.cpp
@@ -4,18 +4,16 @@ using namespace std;
 int main()
 {
 	int n;
-	int round = 1;
+	int round = 0;
 	int a, b;
-	cin >> n;
-	cin >> a >> b;
+	int bin=1, atd = 0;
+	cin >> n >> a >> b;
 
-	int r1 = a + (n - 1);
-	int r2 = b + (n - 1);
-
-	while (r1 / 2 != r2 / 2)
+	while (a != b)
 	{
-		r1 /= 2;
-		r2 /= 2;
+		
+		a = (a+1) / 2;
+		b = (b+1) / 2;
 		round++;
 	}
 	cout << round << endl;
